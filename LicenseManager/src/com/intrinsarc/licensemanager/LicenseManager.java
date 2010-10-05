1cbf955760b674be2d451f7414ab6f1d48cfba12
andrew <devnull@localhost>
2010-10-05 21:05:37 +0100

    working on license manager

--- LicenseManager/src/com/intrinsarc/licensemanager/LicenseManager.java
+++ LicenseManager/src/com/intrinsarc/licensemanager/LicenseManager.java
@@ -1,18 +1,27 @@
 package com.intrinsarc.licensemanager;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import java.net.*;
+import java.security.*;
 import java.util.*;
 
 import javax.swing.*;
+
+import org.freehep.swing.*;
 
 import com.jtattoo.plaf.*;
 import com.jtattoo.plaf.luna.*;
 
 public class LicenseManager extends JFrame
 {
-  public static String LICENSE_KEY = "q35p-g5x9-8ftd-z1au";
-  public static Font DEFAULT_FONT = new Font("Arial", 12, Font.PLAIN);
+  public static final String LICENSE_KEY = "q35p-g5x9-8ftd-z1au";
+  public static final Font DEFAULT_FONT = new Font("Arial", 12, Font.PLAIN);
+  public static final String ALGORITHM = "RSA";
+  public static final int KEY_BITS = 1024;
+  private PrivateKey privateKey;
+  private PublicKey publicKey;
 
 	public static void main(String args[]) throws Exception
 	{
@@ -24,6 +33,87 @@ public class LicenseManager extends JFrame
 		mgr.setVisible(true);
 	}
 
+	private class LoadKeyPairAction extends AbstractAction
+	{
+		JFrame parent;
+		
+		public LoadKeyPairAction(JFrame parent)
+		{
+			super("Load keypair");
+			this.parent = parent;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			JFileChooser chooser = new JFileChooser();
+			chooser.setDialogTitle("Select the keypair file to open...");
+	    ExtensionFileFilter filter = new ExtensionFileFilter();
+	    filter.addExtension("keypair");
+			chooser.setFileFilter(filter);
+			chooser.showOpenDialog(parent);
+
+			File file = chooser.getSelectedFile();
+			if (file != null)
+			{
+//				FileReader reader = new FileReader(file);
+			}
+		}
+	}
+	
+	private class GenerateKeypairAction extends AbstractAction
+	{
+		JFrame parent;
+		
+		public GenerateKeypairAction(JFrame parent)
+		{
+			super("Generate keypair");
+			this.parent = parent;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			KeyPairGenerator keyGen;
+			try
+			{
+				keyGen = KeyPairGenerator.getInstance("RSA");
+				keyGen.initialize(1024);
+				KeyPair pair = keyGen.generateKeyPair();
+				
+				// write out to a file
+				JFileChooser chooser = new JFileChooser();
+				chooser.setDialogTitle("Choose the file to save the keypair to...");
+		    ExtensionFileFilter filter = new ExtensionFileFilter();
+		    filter.addExtension("keypair");
+				chooser.setFileFilter(filter);
+				chooser.showSaveDialog(parent);
+
+				File file = chooser.getSelectedFile();
+				if (!file.getName().endsWith(".keypair"))
+					file = new File(file.getCanonicalPath() + ".keypair");
+				if (file != null)
+				{
+					FileWriter out = new FileWriter(file);
+					out.write(pair.getPrivate().getFormat() + "\n");
+					out.write(toHex(pair.getPrivate().getEncoded()) + "\n");
+					out.write(pair.getPublic().getFormat() + "\n");
+					out.write(toHex(pair.getPublic().getEncoded()) + "\n");
+					out.close();
+					JOptionPane.showMessageDialog(parent, "Keypair written successfully to " + file);
+				}
+			}
+			catch (NoSuchAlgorithmException e1)
+			{
+				JOptionPane.showMessageDialog(parent, "Cannot find key gen algorithm: " + ALGORITHM, null, JOptionPane.ERROR_MESSAGE);
+			}
+			catch (IOException ex)
+			{
+				JOptionPane.showMessageDialog(parent, "Cannot write to selected file", null, JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+	
 	public LicenseManager()
 	{
 		super("Evolve license manager");
@@ -32,10 +122,10 @@ public class LicenseManager extends JFrame
 		JMenuBar menuBar = new JMenuBar();
 		JMenu keys = new JMenu("Keys");
 		menuBar.add(keys);
-		JMenuItem load = new JMenuItem("Load keyset");
+		JMenuItem load = new JMenuItem(new LoadKeyPairAction(this));
 		keys.add(load);
 		keys.addSeparator();
-		JMenuItem gen = new JMenuItem("Generate keyset");
+		JMenuItem gen = new JMenuItem(new GenerateKeypairAction(this));
 		keys.add(gen);
 		
 		JMenu licenses = new JMenu("Licenses");
@@ -63,6 +153,7 @@ public class LicenseManager extends JFrame
 		UIManager.setLookAndFeel(LunaLookAndFeel.class.getName());
 		// change the look and feel
 		JFrame.setDefaultLookAndFeelDecorated(false);
+		JDialog.setDefaultLookAndFeelDecorated(false);
 		
     Properties props = new Properties();
     props.put("logoString", "");
@@ -93,5 +184,21 @@ public class LicenseManager extends JFrame
 		if (resource == null)
 			System.err.println("Cannot find image " + iconName);
 		return new ImageIcon(resource);
+	}
+	
+	public static String toHex(byte[] bytes)
+	{
+  	String hex = "";
+  	for (byte b : bytes)
+  		hex += toHex(b);
+  	return hex;
+	}
+	
+	private static final String[] HEX_CHARS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+	private static String toHex(byte b)
+	{
+		if (b < 0)
+			b += 256;
+		return HEX_CHARS[(b&255)/16] + HEX_CHARS[b&15];
 	}
 }
